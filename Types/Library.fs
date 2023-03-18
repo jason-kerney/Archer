6e929c72907a5ae2875965565b49f1ed2d026b22
jason-kerney <jason@teamkerney.com>
2023-03-18 00:20:03 -0700

    feat: updated types

--- Types/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Archer.Types
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- Types/Types.fs
+++ Types/Types.fs
@@ -4,16 +4,14 @@ open System
 open System.ComponentModel
 open Archer.Results
 
-type TestCancelEventArgs (cancel: bool, result: TestResult option) =
+type TestCancelEventArgsWithResults (cancel: bool, result: TestResult) =
     inherit CancelEventArgs (cancel)
     
-    new () = TestCancelEventArgs (false, None)
-    new (result: TestResult option) = TestCancelEventArgs (false, result)
-    new (result: TestResult) = TestCancelEventArgs (false, Some result)
-    new (cancel: bool, result: TestResult) = TestCancelEventArgs (cancel, Some result)
+    new () = TestCancelEventArgsWithResults (false, TestSuccess)
+    new (result: TestResult) = TestCancelEventArgsWithResults (false, result)
     
     member _.TestResult with get () = result
-    
+  
 type TestEventArgs (result: TestResult option) =
     inherit EventArgs()
     
@@ -22,35 +20,38 @@ type TestEventArgs (result: TestResult option) =
     
     member _.TestResult with get () = result
 
-type CancelDelegate = delegate of obj * TestCancelEventArgs -> unit
+type CancelTestDelegate = delegate of obj * TestCancelEventArgsWithResults -> unit
+type CancelDelegate = delegate of obj * CancelEventArgs -> unit
 type Delegate = delegate of obj * TestEventArgs -> unit
 
-and ITest =
+type ITestExecutor =
     [<CLIEvent>]
-    abstract member StartExecution: IEvent<CancelDelegate, TestCancelEventArgs> with get
+    abstract member StartExecution: IEvent<CancelDelegate, CancelEventArgs> with get
     
     [<CLIEvent>]
-    abstract member StartSetup: IEvent<CancelDelegate, TestCancelEventArgs> with get
+    abstract member StartSetup: IEvent<CancelDelegate, CancelEventArgs> with get
     [<CLIEvent>]
-    abstract member EndSetup: IEvent<CancelDelegate, TestCancelEventArgs> with get
+    abstract member EndSetup: IEvent<CancelTestDelegate, TestCancelEventArgsWithResults> with get
     
     [<CLIEvent>]
-    abstract member StartTest: IEvent<CancelDelegate, TestCancelEventArgs> with get
+    abstract member StartTest: IEvent<CancelDelegate, CancelEventArgs> with get
     [<CLIEvent>]
-    abstract member EndTest: IEvent<CancelDelegate, TestCancelEventArgs> with get
+    abstract member EndTest: IEvent<CancelTestDelegate, TestCancelEventArgsWithResults> with get
     
     [<CLIEvent>]
-    abstract member StartTearDown: IEvent<CancelDelegate, TestCancelEventArgs> with get
-    [<CLIEvent>]
-    abstract member EndTearDown: IEvent<Delegate, TestEventArgs> with get
+    abstract member StartTearDown: IEvent<CancelDelegate, CancelEventArgs> with get
     
     [<CLIEvent>]
     abstract member EndExecution: IEvent<Delegate, TestEventArgs> with get
-    
+
+    abstract member Execute: unit -> TestResult
+
+type ITest =
     abstract member ContainerFullName: string with get
     abstract member ContainerName: string with get
     abstract member TestFullName: string with get
     abstract member TestName: string with get
+    abstract member GetExecutor: unit -> ITestExecutor 
     
 type TestTiming = {
     Setup: TimeSpan
