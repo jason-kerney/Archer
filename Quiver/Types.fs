68b3afd8ff5112b750c71b92f20cbff139bd13d3
jason-kerney <jason.kerney@edf-re.com>
2023-03-21 16:16:40 -0700

    feat: exploring making a test adapter

---
+++ Quiver/Types.fs
@@ -0,0 +1,29 @@
+﻿namespace Archer.Quiver
+
+open Microsoft.VisualStudio.TestPlatform.ObjectModel
+open Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter
+open Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging
+
+type Thing2 () =
+    member _.GetStuff (tc: TestCase) =
+        let id = tc.Id
+        let readOnly_Properties = tc.Properties
+        let source = tc.Source
+        let readOnly_traits = tc.Traits
+        let displayName = tc.DisplayName
+        let executorUri = tc.ExecutorUri
+        let LineNumber = tc.LineNumber
+        let codeFilePath = tc.CodeFilePath
+        let fullQualifiedName = tc.FullyQualifiedName
+        let localExtensionData = tc.LocalExtensionData
+        tc.
+        
+        ()
+        
+
+
+[<FileExtension ".dll">]
+[<FileExtension ".exe">]
+type Thing () =
+    interface ITestDiscoverer with
+        member _.DiscoverTests (containers: string seq, discoveryContext: IDiscoveryContext, logger: IMessageLogger, discoverySink: ITestCaseDiscoverySink) = ()
