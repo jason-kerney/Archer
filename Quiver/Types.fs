c4025eb5dd64335fe8773a47d2e4911bccde227a
jason-kerney <jason.kerney@edf-re.com>
2023-03-22 10:14:30 -0700

    feat: added both adapter interfaces

--- Quiver/Types.fs
+++ Quiver/Types.fs
@@ -4,26 +4,16 @@ open Microsoft.VisualStudio.TestPlatform.ObjectModel
 open Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter
 open Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging
 
-type Thing2 () =
-    member _.GetStuff (tc: TestCase) =
-        let id = tc.Id
-        let readOnly_Properties = tc.Properties
-        let source = tc.Source
-        let readOnly_traits = tc.Traits
-        let displayName = tc.DisplayName
-        let executorUri = tc.ExecutorUri
-        let LineNumber = tc.LineNumber
-        let codeFilePath = tc.CodeFilePath
-        let fullQualifiedName = tc.FullyQualifiedName
-        let localExtensionData = tc.LocalExtensionData
-        tc.
-        
-        ()
-        
-
-
 [<FileExtension ".dll">]
 [<FileExtension ".exe">]
-type Thing () =
+[<DefaultExecutorUri "executor://Archer.Quiver.Executor">]
+type QuiverDiscoverer () =
     interface ITestDiscoverer with
-        member _.DiscoverTests (containers: string seq, discoveryContext: IDiscoveryContext, logger: IMessageLogger, discoverySink: ITestCaseDiscoverySink) = ()
+        member _.DiscoverTests (containers: string seq, discoveryContext: IDiscoveryContext, logger: IMessageLogger, discoverySink: ITestCaseDiscoverySink) = failwith "todo"
+        
+[<ExtensionUri "executor://Archer.Quiver.Executor">]
+type QuiverExecutor () =
+    interface ITestExecutor with
+        member this.RunTests (tests: System.Collections.Generic.IEnumerable<TestCase>, runContext: IRunContext, frameworkHandle: IFrameworkHandle): unit = failwith "todo"
+        member this.Cancel () = failwith "todo"
+        member this.RunTests (sources: System.Collections.Generic.IEnumerable<string>, runContext: IRunContext, frameworkHandle: IFrameworkHandle): unit = failwith "todo"
